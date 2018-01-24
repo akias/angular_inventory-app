c496cc572557e1d7c9515d1ca16f9737e7c7e083
akias <saikam5yday@gmail.com>
2018-01-24 18:15:53 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  product: Product;
+
+  constructor() {
+
+    const newProduct = new Product(
+          'NICEHAT',
+          'A Nice Black Hat',
+          '/resources/images/products/black-hat.jpg',
+          ['Men', 'Accessories', 'Hats'],
+          29.99);
+
+    this.product = newProduct;
+  }
+}
+
+/**
+* Provides a `Product` Object
+*/
+
+class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ) {
+  }
 }
